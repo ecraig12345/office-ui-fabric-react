f950f57e2fff258e4913defa9e179d3d2c05641a
Elizabeth Craig <elcraig@microsoft.com>
2019-04-19 14:45:57 -0700

    Delete unused things from example-app-base

--- packages/example-app-base/src/index.ts
+++ packages/example-app-base/src/index.ts
@@ -8,12 +8,10 @@ export * from './components/Header/index';
 export * from './components/Highlight/Highlight';
 export * from './components/MarkdownTable/index';
 export * from './components/PropertiesTable/index';
-export * from './components/templates/index';
-export * from './utilities/beep';
+export * from './components/PageMarkdown/index';
 export * from './utilities/createApp';
 export * from './utilities/customizations';
 export * from './utilities/data';
-export * from './utilities/debugging';
 export * from './utilities/examplesOf';
 export * from './utilities/parser/index';
 
