fbf97cb9555db7dab6776ab1decce4dfffe22bbc
Elizabeth Craig <elcraig@microsoft.com>
2019-08-12 14:17:48 -0700

    Simplify Button examples

--- apps/fabric-website-resources/src/components/pages/ButtonPage.tsx
+++ apps/fabric-website-resources/src/components/pages/ButtonPage.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import { ButtonPageProps } from 'office-ui-fabric-react/lib/components/Button/Button.doc';
 import { DemoPage } from '../DemoPage';
-import { Checkbox, ICheckboxStyles } from 'office-ui-fabric-react/lib/Checkbox';
+import { Toggle, IToggleStyles } from 'office-ui-fabric-react/lib/Toggle';
 import { IStyleSet } from 'office-ui-fabric-react/lib/Styling';
 
-const checkboxStyles: Partial<IStyleSet<ICheckboxStyles>> = {
+const toggleStyles: Partial<IStyleSet<IToggleStyles>> = {
   root: { margin: '10px 0' }
 };
 
@@ -42,8 +42,8 @@ export class ButtonPage extends React.Component<IButtonDemoPageProps, IButtonDem
     const { areButtonsDisabled, areButtonsChecked } = this.state;
     return (
       <>
-        <Checkbox styles={checkboxStyles} label="Disable buttons" checked={areButtonsDisabled} onChange={this._onDisabledChanged} />
-        <Checkbox styles={checkboxStyles} label="Mark as checked" checked={areButtonsChecked} onChange={this._onToggledChanged} />
+        <Toggle styles={toggleStyles} label="Disable buttons" inlineLabel checked={areButtonsDisabled} onChange={this._onDisabledChanged} />
+        <Toggle styles={toggleStyles} label="Mark as checked" inlineLabel checked={areButtonsChecked} onChange={this._onToggledChanged} />
       </>
     );
   }
